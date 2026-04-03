ce90ebc810221a7c3b4701e3d0405571cbde036a
Richard <riicchhaarrd@users.noreply.github.com>
2026-04-03 15:03:32 -0400

    test

--- asmcheck.py
+++ asmcheck.py
@@ -32,7 +32,7 @@ typedef __builtin_va_list va_list;
 #define va_start(ap, last) __builtin_va_start(ap, last)
 #define va_end(ap) __builtin_va_end(ap)
 #define va_arg(ap, type) __builtin_va_arg(ap, type)
-typedef int BOOL; typedef int Bool; typedef int qboolean;
+typedef int BOOL; typedef int Bool; typedef int qboolean; typedef int bool;
 typedef unsigned int DWORD; typedef unsigned int UINT;
 typedef float vec_t; typedef vec_t vec3_t[3]; typedef vec_t vec2_t[2];
 typedef unsigned char byte;
